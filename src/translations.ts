9e89300ee51ab6d828721cb36f29a0f08bbb9ef9
Johann Hallgrimsson <forritarinn@gmail.com>
2026-04-15 17:00:08 +0000

    routing síðum bætt við

--- src/translations.ts
+++ src/translations.ts
@@ -112,7 +112,7 @@ export const translations: Record<Language, Translations> = {
     references: {
       title: "Meðmælendur",
       items: [
-        "Gunnhildur - CEO Huxun Simi: 840 4990, Email: gunnhildurarnardottir@ceohuxun.is",
+        "Gunnhildur - CEO Huxun Simi: 840 4990, Email: gunnhildurarnar@ceohuxun.is",
         "Ingimar Arndal - One Systems Simi 660 8551, Email: one@one.is",
         "Hrafnkell Erlendsson - One Systems Simi: 660 8553, Email: Hrafnkell@OneSystems.is",
         "Eyvindur Tryggvason - LS Retail - Simi: 616 5050",
@@ -191,7 +191,7 @@ export const translations: Record<Language, Translations> = {
     references: {
       title: "References",
       items: [
-        "Gunnhildur - CEO Huxun Phone: 840 4990, Email: gunnhildurarnardottir@ceohuxun.is",
+        "Gunnhildur - CEO Huxun Phone: 840 4990, Email: gunnhildurarnar@ceohuxun.is",
         "Ingimar Arndal - One Systems Phone 660 8551, Email: one@one.is",
         "Hrafnkell Erlendsson - One Systems Phone: 660 8553, Email: Hrafnkell@OneSystems.is",
         "Eyvindur Tryggvason - LS Retail - Phone: 616 5050",
