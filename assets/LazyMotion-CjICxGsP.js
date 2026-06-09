c779ceeee8075eea4f3905a4d7f6916b83e2fbf7
Johann Hallgrimsson <forritarinn@gmail.com>
2026-06-09 23:21:32 +0000

    Updates

---
+++ assets/LazyMotion-CjICxGsP.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/vendor.motion-DrL5DT_c.js","assets/vendor.react-CYguuEQn.js","assets/vendor--8bMFFpY.js"])))=>i.map(i=>d[i]);
+import{_ as i}from"./index-BEPk1C11.js";import{j as o,r as s,i as a}from"./vendor.react-CYguuEQn.js";const n=a.lazy(async()=>{const t=await i(()=>import("./vendor.motion-DrL5DT_c.js"),__vite__mapDeps([0,1,2]));return{default:({children:e,...r})=>a.createElement(t.motion.div,r,e)}});function _(t){return o.jsx(s.Suspense,{fallback:o.jsx("div",{...t}),children:o.jsx(n,{...t})})}export{_ as L};
