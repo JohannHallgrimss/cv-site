156c3ba6408101bce719ae8c2cdb31ed1e27d7d3
johannhallgrimss <forritarinn@gmail.com>
2026-06-15 20:01:52 +0000

    Updates

--- assets/LazyMotion-DH3oFWbz.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/vendor.motion-hUB6dOYb.js","assets/vendor.react-DoCczY8o.js","assets/vendor-CsaxTnL8.js"])))=>i.map(i=>d[i]);
-import{_ as i}from"./index-DDPryCrx.js";import{j as o,r as s,i as a}from"./vendor.react-DoCczY8o.js";const n=a.lazy(async()=>{const t=await i(()=>import("./vendor.motion-hUB6dOYb.js"),__vite__mapDeps([0,1,2]));return{default:({children:e,...r})=>a.createElement(t.motion.div,r,e)}});function _(t){return o.jsx(s.Suspense,{fallback:o.jsx("div",{...t}),children:o.jsx(n,{...t})})}export{_ as L};
